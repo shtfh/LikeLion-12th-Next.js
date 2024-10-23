a2d33812631b078e93c9ee271bbad5b21d45cb1e
shtfh <syj2003826@gmail.com>
2024-10-23 18:29:47 +0900

    ch9:사용자인증

---
+++ yeonjin/ch9/actions/auth-actions.js
@@ -0,0 +1,87 @@
+"use server";
+
+//서버액션 기능을 생성할려고 하닌깐 추가해주깅
+
+import { createUser } from "@/lib/user";
+import { createAuthSession, destroySession } from "@/lib/auth";
+import { hashUserPassword, verifyPassword } from "@/lib/hash";
+
+
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+
+  if (!email.includes("@")) {
+    errors.email = "Please enter a valid email address.";
+  }
+
+  if (password.trim().length < 8) {
+    errors.password = "Password must be at least 8 characters long.";
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return {
+      errors,
+    };
+  }
+
+  const hashedPassword = hashUserPassword(password);
+  try {
+    const id = createUser(email, hashedPassword);
+    await createAuthSession(id);
+    redirect("/training");
+  } catch (error) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return {
+        errors: {
+          email:
+            "It seems like an account for the chosen email already exists.",
+        },
+      };
+    }
+    throw error;
+  }
+}
+
+export async function login(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  const existingUser = getUserByEmail(email);
+
+  if (!existingUser) {
+    return {
+      errors: {
+        email: "Could not authenticaate user, please check your credentials.",
+      },
+    };
+  }
+
+  const isValidPassword = verifyPassword(existingUser.password, password);
+
+  if (!isValidPassword) {
+    return {
+      errors: {
+        password:
+          "Could not authenticaate user, please check your credentials.",
+      },
+    };
+  }
+
+  await createAuthSession(existingUser.id);
+  redirect("/training");
+}
+
+export async function auth(mode, prevState, formData) {
+  if (mode === "login") {
+    return login(prevState, formData);
+  }
+  return signup(prevState, formData);
+}
+
+export async function logout() {
+  await destroySession();
+  redirect("/");
+}
---
+++ yeonjin/ch9/lib/auth.js
@@ -0,0 +1,88 @@
+import { cookies } from "next/headers";
+import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite"; //새 어댑터 객체를 생성할수잇ㅇ므
+import db from "./db";
+import { Lucia } from "lucia";
+
+const adapter = new BetterSqlite3Adapter(db, {
+  user: "users",
+  session: "sessions",
+});
+
+const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+export async function createAuthSession(userId) {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookieattributes
+  );
+}
+
+export async function verifyAuth() {
+  const sessionCookie = cookies().get(lucia.sessionCookieName);
+
+  if (!sessionCookie) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const sessionId = sessionCookie.value;
+
+  if (!sessionId) {
+    return {
+      user: null,
+      session: null,
+    };
+  }
+
+  const result = await lucia.validateSession(sessionId);
+  try {
+    if (result.session && result.session.fresh) {
+      const sessionCookie = lucia.createSessionCookie(result.session.id);
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookieattributes
+      );
+    }
+    if (!result.session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      cookies().set(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookieattributes
+      );
+    }
+  } catch {}
+
+  return result;
+}
+
+export async function destroySession() {
+  const { session } = verifyAuth();
+  if (!session) {
+    return {
+      error: "Unauthorized!",
+    };
+  }
+
+  await lucia.invalidateSession(session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookieattributes
+  );
+}
