41e96afbf7d422107cd3035ed61f41ff417766ff
shtfh <syj2003826@gmail.com>
2024-10-23 18:30:36 +0900

    ch11:페이지 파일 기반 라우팅

---
+++ yeonjin/ch11/pages/clients/[id]/[clientprojectid].js
@@ -0,0 +1,15 @@
+import { useRouter } from "next/router";
+
+function SelectedClientProjectsPage() {
+  const router = useRouter();
+
+  console.log(router.query);
+
+  return (
+    <div>
+      <h1>The Projects Page for a Specific Project for a Selected Client</h1>
+    </div>
+  );
+}
+
+export default SelectedClientProjectsPage;
